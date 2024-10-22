3855ccb78c5b80636dfd78ff774bdc2edaa9cbfb
Dev3sh <61110768+deveshshevde@users.noreply.github.com>
2024-10-22 21:57:06 +0530

    test code

---
+++ Include/Interrupts/IRQ.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "core/core.h"
+#include "libc/Wprintf.h"
+
+#define PLIC 0x0c000000L
+#define PLIC_PRIORITY (PLIC + 0x0)
+#define PLIC_PENDING (PLIC + 0x1000)
+#define PLIC_SENABLE(hart) (PLIC + 0x2080 + (hart)*0x100)
+#define PLIC_SPRIORITY(hart) (PLIC + 0x201000 + (hart)*0x2000)
+#define PLIC_SCLAIM(hart) (PLIC + 0x201004 + (hart)*0x2000)
+void SetupInterrupt();
+void SaveCurrentContext();
+void IRQ_HANDLER();
+void RestoreOldContext();
+void SetupPlic();
